0414ea78fde31cb65c94b91a7bf0532d83930e6f
GeoSaffer <dfouche8@gmail.com>
2019-09-04 08:52:54 +1200

    Add project to source control

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveTCP_Connections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowActiveTcpConnections();
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(".........................................Done.");
+            Console.ReadLine();
+        }
+
+        public static void ShowActiveTcpConnections()
+        {
+            Console.WriteLine("Active TCP Connections");
+            
+            var properties = IPGlobalProperties.GetIPGlobalProperties();
+            var connections = properties.GetActiveTcpConnections();
+            foreach (var c in connections)
+            {
+                if (c.State == TcpState.Established)
+                {
+                    Console.WriteLine($"{c.LocalEndPoint} <==> {c.RemoteEndPoint}");
+                }
+            }
+        }
+    }
+}
